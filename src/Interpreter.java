76604f7c3146345e2441a21700346e5bad2ba77c
SEBASTIEN BRULOTTE <brulotte.sebastien@courrier.uqam.ca>
2024-10-09 14:10:33 -0400

    basic structure

---
+++ src/Interpreter.java
@@ -0,0 +1,7 @@
+public class Interpreter {
+    public int CO;
+    public int nbMouvement;
+    public int nbCollision;
+    public int nbInstructionInterprete;
+    public int[] Mem;
+}
---
+++ src/Tortue.java
@@ -0,0 +1,34 @@
+
+public class Tortue {
+
+    private Processeur processeur = new Processeur();
+    private Vecteur position;
+    private Direction direction;
+
+
+
+    public boolean wallDetected() {
+
+
+        return true;
+    }
+
+    public void turnLeft() {
+
+    }
+    public void turnRight() {
+
+    }
+    public void moveForward() {
+
+    }
+    public static void readProgram() {
+
+    }
+    public void ececuteProgram() {
+
+    }
+    public void setPosition(int x, int y){
+        position = new Vecteur(x, y);
+    }
+}
